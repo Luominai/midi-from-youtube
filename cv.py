4c48ee187c57807b5923f902748dcffd1a5d3fed
Luominai <kevinthepear@gmail.com>
2026-03-19 17:01:51 -0400

    gitignore

--- cv.py
+++ cv.py
@@ -128,5 +128,5 @@ def process(frame: MatLike): # type: ignore
     return
 
 
-setup_video_capture(process=process, path_to_video="videos/Machine Love - Jamie Paige (Piano Tutorial) [PO0gU5QVKFk].webm")
-# setup_video_capture(process=process, path_to_video="videos/Menu (from Kirby Air Riders) - Piano Tutorial [iElUjQXQkPc].webm")
+# setup_video_capture(process=process, path_to_video="videos/Machine Love - Jamie Paige (Piano Tutorial) [PO0gU5QVKFk].webm")
+setup_video_capture(process=process, path_to_video="videos/Menu (from Kirby Air Riders) - Piano Tutorial [iElUjQXQkPc].webm")
