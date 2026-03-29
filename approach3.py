3b08d3cdc50363b40240aff6683c6b5f1eb7890b
Luominai <kevinthepear@gmail.com>
2026-03-29 19:17:30 -0400

    documenting the most confusing functions

--- approach3.py
+++ approach3.py
@@ -7,7 +7,7 @@ from setup import setup_video_capture
 
 scan_progress = 0
 votes = {}
-vote_threshold = 120
+vote_threshold = 60
 vote_verdict = None
 num_strata = 25
 key_pattern = None
@@ -292,5 +292,5 @@ def cast_vote(votes, valleys, plateaus, full_survey, strata_num):
 
 # setup_video_capture(process, "videos/Machine Love - Jamie Paige (Piano Tutorial) [PO0gU5QVKFk].webm")
 # setup_video_capture(process, "videos/Menu (from Kirby Air Riders) - Piano Tutorial [iElUjQXQkPc].webm")
-# setup_video_capture(process, "videos/Van Gogh by Virginio Aiello, On Piano - [Piano Tutorial] (Synthesia - SeeMusic) [2ESlH-fwxIc].webm")
+setup_video_capture(process, "videos/Van Gogh by Virginio Aiello, On Piano - [Piano Tutorial] (Synthesia - SeeMusic) [2ESlH-fwxIc].webm")
 # setup_video_capture(process, "videos/BIRDBRAIN ｜ Jamie Paige PIANO TUTORIAL SHEET + MIDI [59qdAsKqIjA].webm")
